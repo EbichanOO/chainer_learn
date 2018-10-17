6231b50609bf45c300d598b09de9c2c63d63c572
EbichanOO <takanori.sao@icloud.com>
2018-10-17 17:42:16 +0900

    update

---
+++ seq2seq.py
@@ -0,0 +1,65 @@
+from nltk.translate import bleu_score
+import numpy as np
+import progressbar
+import six
+
+import chainer
+from chainer.backends import cuda
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions as Ex
+
+class Seq2seq(chainer, Chain):
+    def __init__(self, n_layers, n_source_vocab, n_target_vocab, n_units):
+        super(Seq2seq, self).__init__()
+        with self.init_scope():
+            self.embed_x = L.EmbedID(n_source_vocab, n_units)
+            self.embed_y = L.EmbedID(n_target_vocab, n_units)
+            self.encoder = L.NStepLSTM(n_layers, n_units, n_units, 0.1)
+            self.decoder = L.NStepLSTM(n_layers, n_units, n_units, 0.1)
+            self.W = L.Linear(n_units, n_target_vocab)
+        self.n_layers = n_layers
+        self.n_units = n_units
+
+if __name__ == '__main__':
+
+    # training option
+    parser.add_argument('SOURCE', help='source sentence list')
+    parser.add_argument('TARGET', help='target sentence list')
+    parser.add_argument('SOURCE_VOCAB', help='source vocabulary file')
+    parser.add_argument('TARGET_VOCAB', help='target vocabulary file')
+    parser.add_argument('--validation-source',
+                        help='source sentence list for validation')
+    parser.add_argument('--validation-target',
+                        help='target sentence list for validation')
+    parser.add_argument('--batchsize', '-b', type=int, default=64,
+                        help='number of sentence pairs in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20,
+                        help='number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--resume', '-r', default='',
+                        help='resume the training from snapshot')
+    parser.add_argument('--save', '-s', default='',
+                        help='save a snapshot of the training')
+    parser.add_argument('--unit', '-u', type=int, default=1024,
+                        help='number of units')
+    parser.add_argument('--layer', '-l', type=int, default=3,
+                        help='number of layers')
+    parser.add_argument('--min-source-sentence', type=int, default=1,
+                        help='minimium length of source sentence')
+    parser.add_argument('--max-source-sentence', type=int, default=50,
+                        help='maximum length of source sentence')
+    parser.add_argument('--min-target-sentence', type=int, default=1,
+                        help='minimium length of target sentence')
+    parser.add_argument('--max-target-sentence', type=int, default=50,
+                        help='maximum length of target sentence')
+    parser.add_argument('--log-interval', type=int, default=200,
+                        help='number of iteration to show log')
+    parser.add_argument('--validation-interval', type=int, default=4000,
+                        help='number of iteration to evlauate the model '
+                        'with validation dataset')
+    parser.add_argument('--out', '-o', default='result',
+                        help='directory to output the result')
+
